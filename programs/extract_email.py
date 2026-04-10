636a5e1809a1c8caadca2332c364d84c2ce2367c
vilas <vilaskr2962@gmail.com>
2026-04-10 16:50:19 +0530

    Add Program : [ Vilas ] extract emails from the given text

--- programs/extract_email.py
+++ programs/extract_email.py
@@ -1,7 +1,7 @@
 import re
 
 def extract_emails(text: str) -> list[str]:
-    return [x for x in re.findall(r"\w+@\w+.\w{2,}", text)]
+    return [x for x in re.findall(r"\w+@\w+\.\w{2,}", text)]
 
 if __name__ == "__main__":
     text = "Contact: test@gmail.com, admin@yahoo.in, invalid@com, user@domain.co"
